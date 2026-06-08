420ca43d173117309392f82572e6156dce2fef7b
Rohinipilla <satyapilla2005@gmail.com>
2026-06-08 09:57:12 +0530

    Initial commit

---
+++ Java/Sample/objectCreation.java
@@ -0,0 +1,18 @@
+package Java.Sample;
+
+class MyClass {
+    public void myMethod() {
+        System.out.println("This is a method in MyClass.");
+    }
+}
+public class objectCreation {
+   
+
+    public static void main(String[] args) {
+        // Create an object of the class
+        MyClass myObject = new MyClass();
+
+        // Call a method on the object
+        myObject.myMethod();
+    }
+}
